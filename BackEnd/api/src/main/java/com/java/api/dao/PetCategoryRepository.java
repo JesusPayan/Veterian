c5acd3de3337d9bb99969f979250268491a95604
JesusPayan <jesus.he.payan@gmail.com>
2022-10-24 11:53:06 -0600

    Auto-generated commit [ci skip]

--- BackEnd/api/src/main/java/com/java/api/dao/PetCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.java.api.dao;
-
-import com.java.api.model.PetCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetCategoryRepository extends JpaRepository<PetCategory,Long> {
-}
